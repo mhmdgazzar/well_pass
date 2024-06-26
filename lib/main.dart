8e66fd7faecad420368488e26765e024fa98b2a5
mhmdgazzar <mhmdgazzar@gmail.com>
2024-06-26 11:58:46 +0200

    firebase-core-auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:well_pass/firebase_options.dart';
 import 'package:well_pass/src/app.dart';
 import 'package:well_pass/src/data/database_repository.dart';
 import 'package:well_pass/src/data/mock_database.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   DatabaseRepository mockDB = MockDatabase();
   runApp(App(mockDB));
 }
